e83db1366fe74104ce186e34cc8284ebbf960aa5
Adam A <adam@hack.bg>
2022-04-27 16:18:21 +0300

    wip: test(scrt-next): add test suite for ScrtRPCAgent

--- packages/TestRunner.js
+++ packages/TestRunner.js
@@ -1,4 +1,10 @@
-import runTest from '@hackbg/runspec'
-import suites from './ops/index.spec.js.md'
-import scrtSuites from './scrt/Scrt.spec.ts.md'
-runTest({...suites, ...scrtSuites}, process.argv.slice(2))
+import runTest        from '@hackbg/runspec'
+import suites         from './ops/index.spec.js.md'
+import scrtSuites     from './scrt/Scrt.spec.ts.md'
+import scrtNextSuites from './scrt-next/ScrtNext.spec.ts.md'
+
+runTest({
+  ...suites,
+  ...scrtSuites,
+  ...scrtNextSuites,
+}, process.argv.slice(2))
